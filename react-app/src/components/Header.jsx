e318cbede79b56bba70c26a2a7a8b79ba9905fcc
Mayderking <163465498+Mayderking@users.noreply.github.com>
2024-11-19 21:32:55 -0500

    header
    se agrega informacion en el header

--- react-app/src/components/Header.jsx
+++ react-app/src/components/Header.jsx
@@ -1,15 +1,32 @@
 import { Link } from "react-scroll";
+import { AiOutlineGithub, AiOutlineInstagram } from "react-icons/ai";
 
 const Header = () => {
   return (
     <header>
-      <div className="container justify-between max-w-6xl mx-auto md-flex">
+      <div className="container flex justify-between max-w-6xl mx-auto md-flex">
         <Link href="/">
           <h1 className=" my-3 text-4xl font-bold text-center md:text-left text-primary">
             Santiago Grisales
             <span className="text-secondary">.</span>
           </h1>
         </Link>
+        <div className="flex items-center justify-center gap-7">
+          <a
+            href="https://github.com/Mayderking"
+            className="transition-all duration-300 text-primary hover:text-secondary"
+            target="_blank"
+          >
+            <AiOutlineGithub className="size-10" />
+          </a>
+          <a
+            href="https://instagram.com"
+            className="transition-all duration-300 text-primary hover:text-secondary"
+            target="_blank"
+          >
+            <AiOutlineInstagram className="size-10" />
+          </a>
+        </div>
       </div>
     </header>
   );
--- react-app/src/components/Navbar.jsx
+++ react-app/src/components/Navbar.jsx
@@ -4,21 +4,21 @@ import { Link } from "react-scroll";
 const Navbar = () => {
   return (
     <div className="fixed z-40 flex flex-col items-center justify-center w-full h-max bottom-10 text-primary">
-      <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-primary/15 backdrop-blur-sm sm:bg-transparent text-gray-200">
-        <ul className="hidden sm:flex md:flex gap-5 z-10 cursor-pointer">
+      <div className="flex items-center justify-center gap-2 px-4 py-1 rounded-full bg-primary/15 backdrop-blur-sm text-gray-200">
+        <ul className="hidden sm:flex gap-5 z-10 cursor-pointer">
           <li className="px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary">
             <Link to="habilidades">
-              <VscAccount className="size-5" />
+              <VscAccount className="size-8" />
             </Link>
           </li>
           <li className="px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary">
             <Link to="Acerca">
-              <VscBook className="size-5" />
+              <VscBook className="size-8" />
             </Link>
           </li>
           <li className="px-3 py-2 transition duration-150 rounded-full cursor-pointer hover:bg-secondary">
             <Link to="contact">
-              <VscChecklist className="size-5" />
+              <VscChecklist className="size-8" />
             </Link>
           </li>
         </ul>
